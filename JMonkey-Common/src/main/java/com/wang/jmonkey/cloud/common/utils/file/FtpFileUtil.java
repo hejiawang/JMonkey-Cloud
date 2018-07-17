aa474386244dcc2b660c9de658c99c4d6293f09b
赫家旺 <952327407@qq.com>
2018-07-17 17:45:36 +0800

    ftp

--- JMonkey-Common/src/main/java/com/wang/jmonkey/cloud/common/utils/file/FtpFileUtil.java
+++ JMonkey-Common/src/main/java/com/wang/jmonkey/cloud/common/utils/file/FtpFileUtil.java
@@ -1,12 +1,90 @@
 package com.wang.jmonkey.cloud.common.utils.file;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * @Description: FTP操作文件工具类
  * @Auther: HeJiawang
  * @Date: 2018/7/16
  */
+@Slf4j
+@Component
 public class FtpFileUtil {
 
+    @Value("${JMonkey.ftp.host}")
+    private static String host;
 
+    @Value("${JMonkey.ftp.post}")
+    private static int post;
+
+    @Value("${JMonkey.ftp.userName}")
+    private static String userName;
+
+    @Value("${JMonkey.ftp.password}")
+    private static String password;
+
+    /**
+     * 初始化ftp连接信息
+     * @return
+     */
+    public static FTPClient initFtp(){
+        System.out.println(host);
+
+        FTPClient ftp;
+        try {
+            ftp = new FTPClient();
+            ftp.setControlEncoding("UTF-8");
+            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+            ftp.connect(host, post);// 连接FTP服务器
+            ftp.login(userName, password);// 登录
+
+            if ( !FTPReply.isPositiveCompletion( ftp.getReplyCode() ) ){
+                ftp.disconnect();
+                ftp = null;
+            }
+        } catch (IOException e) {
+            log.error("ftp init error : ", e);
+            ftp = null;
+        }
+        return ftp;
+    }
+
+    /**
+     * 上传文件
+     * @return
+     */
+    public static boolean uploadFile( String ftpFilePath, String ftpFileName, InputStream input ) {
+        boolean result = false;
+
+        FTPClient ftp = FtpFileUtil.initFtp();
+        if( null == ftp ) return result;
+
+        try {
+            ftp.makeDirectory(ftpFilePath);
+            ftp.changeWorkingDirectory(ftpFilePath);
+            ftp.storeFile(ftpFileName, input);
+            input.close();
+            ftp.logout();
+        } catch (IOException e) {
+            log.error("ftp init uploadFile : ", e);
+        }  finally {
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                } catch (IOException ioe) {
+                    log.error("ftp init uploadFile disconnect : ", ioe);
+                }
+            }
+        }
+
+        return result;
+    }
 
 }
---
+++ JMonkey-Modules/JMonkey-Modules-UPMS/src/test/java/com/wang/jmonkey/cloud/modules/upms/FTPTest.java
@@ -0,0 +1,34 @@
+package com.wang.jmonkey.cloud.modules.upms;
+
+import com.wang.jmonkey.cloud.common.utils.file.FtpFileUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * @Description:
+ * @Auther: HeJiawang
+ * @Date: 2018/7/17
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = JMonkeyUPMSApplication.class)
+public class FTPTest {
+
+    @Test
+    public void treeTest(){
+
+        try {
+            InputStream input = new FileInputStream( new File("D:/nihao.txt"));
+            FtpFileUtil.uploadFile("/", "nihao.txt", input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
